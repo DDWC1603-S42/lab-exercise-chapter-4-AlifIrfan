752fdbc11fc4d62501d24fcd816f8c70a653ce68
AlifIrfan <35548305+AlifIrfan@users.noreply.github.com>
2018-01-18 10:20:19 +0800

    dis is not da we yu deeval

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -1,24 +1,26 @@
-//Display the pattern using numebr repeating for a row
-//fix and complete the program below.
-
-
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
+	int i,j,row,a,b;
+	
 	cout<<"Display the pattern"<<endl;
 	cout<<"-------------------"<<endl;
 
-	i,j,rows;
+	i,j;
 	cout<<"Input number of rows :"<<endl;
+	cin>>row;
 
-
-	for(i=1;i<=rows;i++)
+	for(i=1;i<=row;i++)
 	{
+		for(b=a;b<=1;b++)
+		{
+			cout<<" "<<endl;
+		}
 	for(j=1;j<=i;j++)
-		
+	cout<<"*";
 	cout<<endl;
+	a--;
 	}
 }
-	
