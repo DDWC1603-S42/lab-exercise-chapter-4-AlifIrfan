dd6e4bdd99f8233554603a8e35c994128b12382a
AlifIrfan <35548305+AlifIrfan@users.noreply.github.com>
2018-01-18 10:06:14 +0800

    i will show yu da wae

--- Lab-Exercise-1.cpp
+++ Lab-Exercise-1.cpp
@@ -1,19 +1,20 @@
-//Find the sum of digits of a given number
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
 
-	int no1,no2,x,sum;
+	int no1,no2,r,x,sum;
 
 	cout<<"Find the sum of a given number"<<endl;
 	cout<<"------------------------------"<<endl;
 
 	cout<<"Input number: "<<endl;
-
-
+	cin>>no1;
+	
+	cout<<"Input number: "<<endl;
+	cin>>no2;
+	
 	while(no2>0)
 	{
 	r=no1%10;
